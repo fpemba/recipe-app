414604d5bca8972f0e366bc016f3f803c4c5da1d
fpemba <fatuma.pemba@yahoo.com>
2020-07-20 20:31:31 -0400

    adding index controller and html page

--- src/main/java/com/fpemba/recipeproject/RecipeprojectApplication.java
+++ src/main/java/com/fpemba/recipeproject/RecipeprojectApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RecipeprojectApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(RecipeprojectApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeprojectApplication.class, args);
+	}
 
 }
